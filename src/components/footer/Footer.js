56207c75659b5610ff7cbe462d47236367d8b59c
Nihal Thangallapally <146743988+nihal1521@users.noreply.github.com>
2025-10-24 20:46:10 +0000

    github link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
-            href="#"
+            href="https://github.com/nihal1521"
             target="_blank"
             rel="noreferrer"
           >
